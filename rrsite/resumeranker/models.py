c49fc68fad3376a80d01df411e350efee645b5f5
Divyam Prajapati <divyambp@somaiya.edu>
2022-04-10 15:52:48 +0530

    Major Done

--- rrsite/resumeranker/models.py
+++ rrsite/resumeranker/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
 # Create your models here.
+
+class myUploadFile(models.Model):
+    myfiles = models.FileField(upload_to="")
+
+class Data(models.Model):
+    text = models.TextField()
+    name = models.CharField(max_length=64, null=True)
+    collegeName = models.CharField(max_length=100, null=True) 
+    degree = models.CharField(max_length=200, null=True)
+    graduationYear = models.CharField(max_length=200, null=True)
+    yearsOfExperience = models.CharField(max_length=200, null=True)
+    companiesWorkedAt = models.CharField(max_length=200, null=True)
+    designation = models.CharField(max_length=200, null=True)
+    skills = models.TextField(max_length=255, null=True)
+    Location = models.TextField(max_length=200, null=True)
+    emailAddress = models.EmailField(null=True)
